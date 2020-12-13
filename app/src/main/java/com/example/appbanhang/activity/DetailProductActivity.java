585f072ecb9ab9123dab0c25ba852af204440a86
KhanhLy <18521079@gm.uit.edu>
2020-12-12 21:58:50 -0800

    update product details (done)

--- app/src/main/java/com/example/appbanhang/activity/DetailProductActivity.java
+++ app/src/main/java/com/example/appbanhang/activity/DetailProductActivity.java
@@ -1,16 +1,30 @@
 package com.example.appbanhang.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.example.appbanhang.R;
 
 public class DetailProductActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_product2);
+        anhXa();
+        actionToolBar();
     }
+
+    private void actionToolBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void anhXa() {
+        toolbar= (Toolbar) findViewById(R.id.toolbar);
+    }
+
 }
